ba18944e7a2095612b7dc512c79435ef7c79e99a
anny.chen <anny.chen@tenoz.tw>
2020-11-18 23:47:38 +0800

    iOS 如何選擇照片與拍照 - UIImagePickerController

--- PhotoSelect/ViewController.swift
+++ PhotoSelect/ViewController.swift
@@ -8,13 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    // 按下 Select 按鈕後需要執行選取圖片
+    @IBAction func selectPhoto(_ sender: UIButton) {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        
+        let alertController = UIAlertController(title: "選擇開啟圖片方式", message: "", preferredStyle: .actionSheet)
+        
+        let photoAlert = UIAlertAction(title: "從相簿開啟", style: .default) { (_) in
+            pickerController.sourceType = .savedPhotosAlbum
+            self.present(pickerController, animated: true, completion: nil)
+        }
+        
+        let cameraAlert = UIAlertAction(title: "從相機拍攝", style: .default) { (_) in
+            pickerController.sourceType = .camera
+            self.present(pickerController, animated: true, completion: nil)
+        }
+            
+        let cancelAction = UIAlertAction(title: "取消", style: .cancel, handler: nil)
+        
+        alertController.addAction(photoAlert)
+        alertController.addAction(cameraAlert)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.originalImage] as? UIImage
+        if let image = image{
+            imageView.image = image
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
 }
 
